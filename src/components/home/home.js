e7efc092303bcf58b5a666df8d0f7556791e77f9
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-02-28 14:48:31 -0500

    created home page scaffolding grid

---
+++ src/components/home/home.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Jumbotron, Grid, Row, Col, Image, Button } from "react-bootstrap";
+import "./home.css";
+
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <Grid>
+        <Jumbotron>
+          <h2>
+            <p>
+              {" "}
+              Bracket Factory Built on ReactJS, React-Bootstrap and React-Router{" "}
+            </p>
+          </h2>
+        </Jumbotron>
+      </Grid>
+    );
+  }
+}
+
+export default Home;
