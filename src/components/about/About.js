44810bcb4d625ac6c1a17c67c4a8157eee155556
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-02-28 14:53:55 -0500

    created blog and about page scaffolding

---
+++ src/components/about/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import React from "react-bootstrap";
+
+class About extends Component {
+  state = {};
+  render() {
+    return <div>About</div>;
+  }
+}
+
+export default About;
---
+++ src/components/blog/Blog.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import {} from "react-bootstrap";
+
+class Blog extends Component {
+  state = {};
+  render() {
+    return <div>Blog</div>;
+  }
+}
+
+export default Blog;
